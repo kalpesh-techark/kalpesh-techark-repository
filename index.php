3e7d02b8890a1be5b5c55f9b043aa9b67feee7ff
kalpesh-techark <kmorker.techark@gmail.com>
2020-09-09 16:35:52 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+ echo "this is my text project index";
+?>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//dd('route');
+//Route::get('/','ArticleController@index');
+// Route::get('/','SendgridController@index');
+// Route::get('/sendgrid/post','SendgridController@create');
+// Route::get('/article/{Id}','ArticleController@view');
+// Route::post('/article/create','ArticleController@create')->name('article.create');
+// Route::put('/article/update/{Id}','ArticleController@update')->name('article.update');
+// Route::delete('/article/delete/{Id}','ArticleController@delete')->name('article.delete');
